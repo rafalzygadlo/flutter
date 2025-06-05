fd8ba080806340028a9c5592cf5b5e5547b3e8ca
rafał żygadło <rafal@zygadlo.org>
2025-06-05 15:26:56 +0200

    Register password icon

---
+++ lib/data/models/register.dart
@@ -0,0 +1,11 @@
+
+class Register {
+  
+  final String email;
+  final String password;
+  
+  Register({
+    required this.email,
+    required this.password,
+  });
+}
--- lib/presentation/pages/register.dart
+++ lib/presentation/pages/register.dart
@@ -1,14 +1,36 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/data/models/register.dart';
 import 'package:flutter_application_2/presentation/pages/menu.dart';
 
 class RegisterPage extends StatefulWidget {
+  
   @override
   RegisterPageState createState() => RegisterPageState();
 }
 
 class RegisterPageState extends State<RegisterPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _obscurePassword = true;
+  
+  @override
+  void initState() {
+    super.initState();
+    
+    //_emailController.addListener(() => setState(() {}));
+    //_passwordController.addListener(() => setState(() {}));
+    
+  }
+  
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
   void _submitForm(BuildContext context) {
+    
     
     /*
     ScaffoldMessenger.of(
@@ -32,7 +54,24 @@ class RegisterPageState extends State<RegisterPage> {
             children: [
               TextFormField(decoration: InputDecoration(labelText: 'email')),
               SizedBox(height: 10),
-              TextFormField(decoration: InputDecoration(labelText: 'password')),
+              TextFormField(
+                controller: _passwordController,
+                obscureText: _obscurePassword,
+                decoration: InputDecoration(
+                  labelText: 'password',
+                  prefixIcon: Icon(Icons.password),
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                        setState(() {
+                          _obscurePassword = !_obscurePassword;
+                        });
+                        }, 
+                        icon: _obscurePassword
+                          ? const Icon(Icons.visibility_outlined)
+                          : const Icon(Icons.visibility_off_outlined))
+                          ),
+                          validator: (String? value) { return null;},
+                  ),
               SizedBox(height: 20),
               OutlinedButton(
                 onPressed: () => _submitForm(context),
